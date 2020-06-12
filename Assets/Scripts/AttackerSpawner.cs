dbca208759c12d161e98cbd13c65fc86a0747ca3
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-06-12 23:29:33 +0300

    Lizard and Trophy with animations added

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 5f;
+    [SerializeField] Attacker attackerPrefab;
+
+
+    bool spawn = true;
+    
+
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}

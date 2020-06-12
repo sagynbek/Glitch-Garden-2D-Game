33f7eb200070c0ed92585b7d31e8166549b89692
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-06-13 00:11:26 +0300

    Cactus firing zucchini

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [Range(0f, 20f)]
+    [SerializeField] float speed = 1f;
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+}

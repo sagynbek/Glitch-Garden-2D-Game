36630e80e8b2e514b8dee9db4c426b2bfbcaa327
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-06-13 13:20:04 +0300

    Spawn Cactus on screen

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject defender;
+    private void OnMouseDown()
+    {
+        SpawnDefender(GetSquarePosition());
+    }
+
+    private Vector2 GetSquarePosition()
+    {
+        Vector2 clickPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPosition);
+        Vector2 gridPos = SnapToGrid(worldPos);
+
+        return gridPos;
+    }
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
+    {
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+
+        return new Vector2(newX, newY);
+    }
+
+    private void SpawnDefender(Vector2 spawnPosition)
+    {
+        GameObject newDefender = Instantiate(defender, spawnPosition, Quaternion.identity) as GameObject;
+    }
+}
